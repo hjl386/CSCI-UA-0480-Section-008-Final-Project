03108497a3cdca0254d2f6739c1d992d660ce93b
Han Jie Lee <hjl386@nyu.edu>
2017-04-08 19:16:30 -0400

    Passed Eslint test

--- db.js
+++ db.js
@@ -42,16 +42,18 @@ User.plugin(URLSlugs('title'));
 if (process.env.NODE_ENV === 'PRODUCTION'){
 	//If we're in PRODUCTION mode, then read the configuration from a file 
 	//Use Blocking file IO to do this ... 
-	var fs = require('fs');
-	var path = require('path');
-	var fn = path.join(__dirname, 'config.json');
- 	var data = fs.readFileSync(fn);
-	 // our configuration file will be in json, so parse it and set the conenction string appropriately!
-	var conf = JSON.parse(data);
-	var dbconf = conf.dbconf;
+	const fs = require('fs');
+	const path = require('path');
+	const fn = path.join(__dirname, 'config.json');
+	const data = fs.readFileSync(fn);
+	// our configuration file will be in json, so parse it and set the conenction string appropriately!
+	const conf = JSON.parse(data);
+	const dbconf = conf.dbconf;
+	mongoose.connect(dbconf);
 } else {
 	// if we're not in PRODUCTION mode, then use
-	dbconf = 'mongodb://localhost/hjl386';
+	const dbconf = 'mongodb://localhost/hjl386';
+	mongoose.connect(dbconf);
 }
 
 mongoose.model('User', User);
@@ -59,4 +61,4 @@ mongoose.model('Match', Match);
 mongoose.model('Review', Review);
 mongoose.model('Critique', Critique);
 
-mongoose.connect(dbconf);
+
